b23c9600eee82fb5572f4108ae4bb8850cc7fddf
Ashwin Ramkumar <ramkin@terpmail.umd.edu>
2026-09-12 12:04:29 -0400

    Adding first HTML page with Hello World message and new route

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -7,6 +6,8 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Ashwin Ramkumar!'
 
-
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 if __name__ == '__main__':
     app.run()
